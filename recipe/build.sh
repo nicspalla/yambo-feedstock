d5e99e90c5d384f8eb3b4803b4a46c010ecb219e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-02-13 13:48:36 +0200

    Pre-build devXlib before Yambo
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,17 @@
 
 set -xe
 
+# Build devxlib
+pushd devxlib
+./configure --prefix="${PREFIX}" \
+    --enable-openmp \
+    --enable-parallel \
+    --with-blas-libs="${PREFIX}/lib/libblas.so" \
+    --with-lapack-libs="${PREFIX}/lib/liblapack.so"
+make -j"${CPU_COUNT}" install
+popd
+
+
 export CPP="${CC} -E -P"
 export FPP="${FC} -E -P -cpp"
 
@@ -10,20 +21,23 @@ export FPP="${FC} -E -P -cpp"
     --enable-mpi --enable-open-mp \
     --with-fft-path="${PREFIX}" \
     --with-hdf5-path="${PREFIX}" \
-    --with-netcdf-path="${PREFIX}" \
-    --with-netcdff-path="${PREFIX}" \
+    --with-netcdf-libs="${PREFIX}/lib/libnetcdf.so" \
+    --with-netcdff-libs="${PREFIX}/lib/libnetcdff.so" \
     --enable-hdf5-par-io \
-    --with-libxc-path="${PREFIX}" \
+    --with-libxc-libdir="${PREFIX}/lib" \
     --with-scalapack-libs="${PREFIX}/lib/libscalapack.so" \
     --with-blacs-libs="${PREFIX}/lib/libscalapack.so" \
     --with-blas-libs="${PREFIX}/lib/libblas.so" \
     --with-lapack-libs="${PREFIX}/lib/liblapack.so" \
+    --with-devxlib-path="${PREFIX}" \
+    --with-iotk-libs="${PWD}/lib/iotk/iotk/src/libiotk.a" \
     --enable-par-linalg \
     --with-slepc-path="${PREFIX}" \
     --with-petsc-path="${PREFIX}" \
-    --enable-slepc-linalg 
+    --enable-slepc-linalg
 
-make -j$CPU_COUNT all || for f in `find ./ -name "*.log"`; do echo "Printing the contents of '$f'"; cat $f; done
+make -j$CPU_COUNT all || (cat log/*yambo*.log && exit 123)
+#for f in `find ./ -name "*.log"`; do echo "Printing the contents of '$f'"; cat $f; done
 
 ls -la $PREFIX/bin
 
