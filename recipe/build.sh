f48c46bed95288651601daa4d24a07ea5909b610
Nicola Spallanzani <nic.spalla@gmail.com>
2025-10-17 12:27:07 +0200

    MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.15.11.56.55
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,11 +23,11 @@ popd
 # # Build iotk
 pushd iotk
 
-#if [[ "${CONDA_BUILD_CROSS_COMPILATION:0}" == "1" ]]; then
-#    sed -i.bak1 's/ -march=[^ ]*//' configure
-#    sed -i.bak2 's/ -mcpu=[^ ]*//' configure
-#    sed -i.bak3 's/ -mtune=[^ ]*//' configure
-#fi
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+    sed -i.bak1 's/ -march=[^ ]*//' configure
+    sed -i.bak2 's/ -mcpu=[^ ]*//' configure
+    sed -i.bak3 's/ -mtune=[^ ]*//' configure
+fi
 
 cp -f ${RECIPE_DIR}/iotk-make.sys ../make.sys
 cp -f ${RECIPE_DIR}/iotk_specials.h include/
@@ -44,11 +44,11 @@ ls -la ${SRC_DIR}/iotk/src/libiotk.a
 
 # export LD="${ORIG_LD}"
 
-#if [[ "${CONDA_BUILD_CROSS_COMPILATION:0}" == "1" ]]; then
-#    sed -i.bak1 's/ -march=[^ ]*//' configure
-#    sed -i.bak2 's/ -mcpu=[^ ]*//' configure
-#    sed -i.bak3 's/ -mtune=[^ ]*//' configure
-#fi
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+    sed -i.bak1 's/ -march=[^ ]*//' configure
+    sed -i.bak2 's/ -mcpu=[^ ]*//' configure
+    sed -i.bak3 's/ -mtune=[^ ]*//' configure
+fi
 sed -i.bak 's/\(test -r \$try_netcdff_libdir\/libnetcdff\.so\)/\1 || test -r \$try_netcdff_libdir\/libnetcdff.dylib/' configure
 
 cp -f ${SRC_DIR}/devxlib/config/config.sub config/
